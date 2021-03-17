a520e8124f53a30f0dc2a1a2d04bdddaea2b2c2e
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2021-03-17 13:09:39 -0400

    Add project files.

---
+++ StatePatternConsoleApp/StatePatternApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatePatternConsoleApp
+{
+   class StatePatternApp
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
