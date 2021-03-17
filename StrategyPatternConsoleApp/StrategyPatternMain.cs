af3665ebec827a940f2e0452ed82029b1cd121a5
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2021-03-17 15:56:49 -0400

    Initial setup for lecture

---
+++ StrategyPatternConsoleApp/StrategyPatternMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StrategyPatternConsoleApp
+{
+   class StrategyPatternMain
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
---
+++ TemplateMethodConsoleApp/TemplateMethodMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplateMethodConsoleApp
+{
+   class TemplateMethodMain
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
